c0ee687c91f1a58c5b3bc1b9466efc28e31460e2
aaryansharmaa <aaryans018@gmail.com>
2024-07-13 10:52:23 +0530

    first commit

---
+++ day_1/var_datatypes.js
@@ -0,0 +1,46 @@
+//task 1
+
+var n = 10;
+console.log(n);
+
+// task 2
+
+let str = "abc";
+console.log(str);
+
+//task 3
+
+const bool = true;
+console.log(bool);
+
+//task 4
+
+let num = 1;
+let s = "abc";
+let x = true;
+const data = {
+  name: "learner",
+  age: 22,
+};
+const arr = [1, 2];
+
+console.log(typeof num);
+console.log(typeof s);
+console.log(typeof x);
+console.log(typeof data);
+console.log(typeof arr);
+
+// task 5
+
+let assign = 5;
+
+console.log(assign);
+
+assign = 10;
+
+// task 6
+console.log(assign);
+
+const constant = 5;
+
+// constant = 10;
